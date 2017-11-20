87bfbca7f6bf41404e7ee88edc7d700e548a32c7
Bobby Bryant <bobby@Bobbys-MacBook-Pro-2.local>
2017-11-20 12:32:06 -0500

    bump vesion

--- omg-forms.php
+++ omg-forms.php
@@ -13,7 +13,7 @@ if ( !defined( 'OMG_FORMS_FILE' ) ) {
 }
 
 if ( !defined( 'OMG_FORMS_VERSION' ) ) {
-	define( 'OMG_FORMS_VERSION', '0.6.3' );
+	define( 'OMG_FORMS_VERSION', '0.7.0-beta1' );
 }
 
 require_once OMG_FORMS_DIR . '/includes/core.php';
